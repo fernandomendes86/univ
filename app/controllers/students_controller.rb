fe4623111f905d816ec9abaa670327821a790a04
Fernando Mendes <fernando.msantos1@gmail.com>
2024-03-05 21:08:34 -0300

    add render form to create and edit student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: [:show]
+  before_action :set_student, only: [:show, :edit, :update]
 
   def index
     @students = Student.all
@@ -12,13 +12,25 @@ class StudentsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
   def create
     @student = Student.new(student_params)
     if @student.save
       flash.notice = "Student #{@student.name.capitalize} was successfully created."
-      redirect_to root_path
+      redirect_to @student
     else
       render 'new'
+    end
+  end
+
+  def update
+    if @student.update(student_params)
+      flash.notice = "Student #{@student.name.capitalize} was successfully updated."
+      redirect_to @student
+    else
+      render 'edit'
     end
   end
 
@@ -31,5 +43,4 @@ class StudentsController < ApplicationController
   def student_params
     params.require(:student).permit(:name, :email)
   end
-
 end
