361f01b1cf337e7202541b3565a393afb7137f44
Fernando Mendes <fernando.msantos1@gmail.com>
2024-03-12 18:34:38 -0300

    login structure

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def errors_for(form, field)
-    content_tag(:p, form.object.errors[field].try(:first), class: 'text-red-800')
+    content_tag(:p, form.object.errors[field].try(:first), class: 'text-red-800') if form.object
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get 'courses/new', to: 'courses#new'
 
   resources :students, except: [:destroy]
+
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'logout', to: 'logins#destroy'
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
