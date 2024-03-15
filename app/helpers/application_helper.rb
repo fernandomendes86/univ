0ef4be3e196e2564c50db92e4359ba04bbf8c755
Fernando Mendes <fernando.msantos1@gmail.com>
2024-03-14 21:13:34 -0300

    session to login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def errors_for(form, field)
     content_tag(:p, form.object.errors[field].try(:first), class: 'text-red-800') if form.object
   end
+
+  def session_link
+    if logged_in?
+      button_to('(logout)', logout_path , method: :delete, class: 'block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4')
+    else
+      button_to('(login)', login_path, method: :get, class: 'block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4')
+    end
+  end
 end
